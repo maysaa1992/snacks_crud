ea979800dd01a9d6f3c3564ee78abea333596852
maysaa1992 <batainehmaysaa@gmail.com>
2023-07-20 19:34:57 +0000

    lastupdat

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,42 +1,40 @@
 from django.test import TestCase
-
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 from .models import Snack
-from django.urls import reverse
 # Create your tests here.
-class Thingtest(TestCase):
+
+class SnackTest(TestCase):
+    def test_list_view_status(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response,'snacks_list.html')
+
+
+
     def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username='random',email='random@random.com',
-            password='random@12345'
+
+        self.user=get_user_model().objects.create_user(
+            username='test',
+            email='test@test.com',
+            password='test')
+
+        self.snacks=Snack.objects.create(
+            title='test',
+            purchaser=self.user,
+            description='test' 
         )
-        self.snack = Snack.objects.create(
-            title = 'test',
-            purchaser=self.user.id,
-            description= 'no desc available'
-        )
+
 
     def test_str_method(self):
-        self.assertEqual(str(self.snack),'test')
+        self.assertEqual(str(self.snacks),'test')
 
     def test_detail_view(self):
-        url = reverse('snack_detail',args=[self.snack.id])  
+        url = reverse('snack_detail',args=[self.snacks.id])
         response = self.client.get(url)
-
-        self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,'snacks_detail.html')
-
-
-    def test_create_view(self):
-        url = reverse('snack_create')
-        data={
-            "title": "test_2",
-            "purchaser" : id,
-            "description": 'no desc available'
-        }
-
-
-        response = self.client.post(path=url,data = data,follow = True)
-        self.assertTemplateUsed(response,'snacks_detail.html')
-        self.assertEqual(len(Snack.objects.all()),2)
-        self.assertRedirects(response, reverse('snack_detail',args=[2]))
